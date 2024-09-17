a9729e2382333ab1964fec621cfbf3875e600e58
unknown <https://github.com/MidooAymam>
2024-09-18 00:46:04 +0300

    Initial commit

---
+++ myaccount/apps.py
@@ -0,0 +1,6 @@
+# myaccount/apps.py
+from django.apps import AppConfig
+
+class MyAccountConfig(AppConfig):  # تأكد أن الكلاس مكتوب بشكل صحيح
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'myaccount'
